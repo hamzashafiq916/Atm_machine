a176ee9ef1cfae714b64dc3158b36700efa103fd
hamzashafiq916 <hamzashafiq916@gmail.com>
2024-04-30 15:48:58 +0500

    atm commit

---
+++ main.js
@@ -0,0 +1,40 @@
+import inquirer from "inquirer";
+let myBalance = 20000; // usd
+let myPincode = 1234;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "enter your pin",
+        type: "number",
+    },
+]);
+console.log(pinAnswer.pin);
+if (pinAnswer.pin === myPincode) {
+    console.log("correct pin code!!!");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "please select option",
+            type: "list",
+            choices: ["withdraw", "check balance"],
+        },
+    ]);
+    console.log(operationAns);
+    if (operationAns.operation === "withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "Please enter amount",
+                type: "number",
+            },
+        ]);
+        // =, -=, +=
+        myBalance -= amountAns.amount;
+        console.log("your remaining balance is: " + myBalance);
+    }
+    else if (operationAns.operation === "check balance") {
+        console.log("your balance is: " + myBalance);
+    }
+}
+else
+    console.log("Incorrect pin code");
